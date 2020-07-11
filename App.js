0a2972f8412b54e15317d8305a8e2fc5dc0ec697
phamtanphat <phatdroid94@gmail.com>
2020-07-11 07:21:35 -0700

    bai tap so 1

--- App.js
+++ App.js
@@ -11,14 +11,37 @@ export default class App extends Component {
             flex : 1,
             flexDirection : 'column',
           }}>
-          <Text 
-            style={{backgroundColor : 'yellow'}}>
-              View 1
-          </Text>
-          <Text 
-            style={{backgroundColor : 'green'}}>
-              View 2
-          </Text>
+          <View style={{backgroundColor : 'red' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >A</Text>
+          </View>
+          <View style={{backgroundColor : 'green' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >B</Text>
+          </View>
+          <View style={{backgroundColor : 'blue' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >C</Text>
+          </View>
+          <View style={{backgroundColor : 'orange' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >D</Text>
+          </View>
+        </View>
+        <View 
+          style={{
+            backgroundColor: 'yellow',
+            flex : 1,
+            flexDirection : 'row',
+          }}>
+          <View style={{backgroundColor : 'red' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >A</Text>
+          </View>
+          <View style={{backgroundColor : 'green' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >B</Text>
+          </View>
+          <View style={{backgroundColor : 'blue' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >C</Text>
+          </View>
+          <View style={{backgroundColor : 'orange' , flex : 1 , justifyContent : 'center' , alignItems : 'center' }}>
+            <Text >D</Text>
+          </View>
         </View>
       </SafeAreaView>
     )
