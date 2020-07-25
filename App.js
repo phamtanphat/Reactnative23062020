03f89578f96f38a09f429e44bdc43b41634ee861
phamtanphat <phatdroid94@gmail.com>
2020-07-25 07:31:43 -0700

    su dung store

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { SafeAreaView , StyleSheet} from 'react-native'
-import Main from './src/screen'
-import Form from './src/component/Form'
+// import Main from './src/screen'
+// import Form from './src/component/Form'
 import Box from './src/component/Box'
+import { createStore } from 'redux'
 // import Filter from './src/component/Filter'
+
+
+// 1 : Tao ra store
+const store = createStore(function(state = 0,action){
+    if(action.type === 'INCREASE'){
+      return state + 1
+    }
+    return state
+})
+
+console.log(store.getState())
+store.dispatch({type : 'INCREASE'})
+console.log(store.getState())
+
+
+
 export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Main/>
+        <Box/>
       </SafeAreaView>
     )
   }
@@ -21,3 +38,4 @@ const styles = StyleSheet.create({
       flex : 1,
     },
 })
+
